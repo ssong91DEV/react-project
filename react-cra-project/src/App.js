31385d114ec92544c5be4904c4970d21d5ae52a2
shb8204@naver.com <shb8204@naver.com>
2022-07-20 10:50:50 +0900

    test 2

--- react-cra-project/src/App.js
+++ react-cra-project/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <div>Hello React</div>
       <div>Hello React</div>
-      <div>new project 0720 test</div>
+      <div>new project 0720 test 2</div>
     </div>
   );
 }
