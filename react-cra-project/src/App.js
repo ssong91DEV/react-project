fcfd0a93930d4a13f52f20e578c327938cdf73fb
shb8204@naver.com <shb8204@naver.com>
2022-07-20 13:48:17 +0900

    20일 테스트1

--- react-cra-project/src/App.js
+++ react-cra-project/src/App.js
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <div>hyeonbeom 브랜치에서 작업한내용</div>
-      <div>추가 작업</div>
-      <div>main에서 추가작업 12시54분</div>
-      <div>hyeonbeom에서 추가작업~ 1</div>
-      <div>메인에서에서 추가작업~ 2</div>
+      <div>20일 테스트1</div>
     </div>
   );
 }
