a4b54198266dc96842acc075dd2aa0fb9093569a
shb8204@naver.com <shb8204@naver.com>
2022-08-02 18:43:49 +0900

    파이프라인 아이템 추가후 커밋

--- src/containers/MainComponent.jsx
+++ src/containers/MainComponent.jsx
@@ -15,6 +15,7 @@ const MainComponent = () => {
       <div>워커노드 생성 후</div>
       <div>이 프로젝트를 react-project라는 라벨을 가진 노드에서 빌드</div>
       <div>테스트</div>
+      <div>파이프라인 실행하기</div>
       <div>
         {time.toLocaleDateString()} {time.toLocaleTimeString()}
       </div>
